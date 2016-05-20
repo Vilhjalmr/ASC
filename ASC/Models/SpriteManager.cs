7dc9afb130f86207555a11131ebd680c9e8733a8
Vilhjalmr <guillaume.danneau@gmail.com>
2016-05-20 15:03:12 +0200

    CH5.Done=true;

--- ASC/Models/SpriteManager.cs
+++ ASC/Models/SpriteManager.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +8,88 @@ using System.Threading.Tasks;
 
 namespace ASC.Models
 {
-    internal class SpriteManager : DrawableGameComponent
+    public class SpriteManager : DrawableGameComponent
     {
-        public SpriteManager(Game game) : base(game)
+        SpriteBatch spriteBatch;
+        UserControlledSprite player;
+        List<Sprite> spriteList = new List<Sprite>();
+
+        public SpriteManager(Microsoft.Xna.Framework.Game game) : base(game)
         { }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(Game.GraphicsDevice);
+
+            player = new UserControlledSprite(
+                Game.Content.Load<Texture2D>("threerings"), 
+                Vector2.Zero, new Point(75, 75), 10, 
+                new Point(0, 0), new Point(6, 8), 
+                new Vector2(6, 6));
+
+            spriteList.Add(new AutomatedSprite(
+                Game.Content.Load<Texture2D>("skullball"),
+                new Vector2(150, 150), new Point(75, 75), 10,
+                new Point(0, 0), new Point(6, 8), Vector2.Zero));
+
+            spriteList.Add(new AutomatedSprite(
+                Game.Content.Load<Texture2D>("skullball"),
+                new Vector2(150, 300), new Point(75, 75), 10,
+                new Point(0, 0), new Point(6, 8), Vector2.Zero));
+
+            spriteList.Add(new AutomatedSprite(
+                Game.Content.Load<Texture2D>("skullball"),
+                new Vector2(300, 150), new Point(75, 75), 10,
+                new Point(0, 0), new Point(6, 8), new Vector2(3,1)));
+
+            spriteList.Add(new AutomatedSprite(
+                Game.Content.Load<Texture2D>("skullball"),
+                new Vector2(600, 300), new Point(75, 75), 10,
+                new Point(0, 0), new Point(6, 8), Vector2.One));
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Update player
+            player.Update(gameTime, Game.Window.ClientBounds);
+
+            // Update sprite list
+            foreach (Sprite s in spriteList)
+            {
+                s.Update(gameTime, Game.Window.ClientBounds);
+
+                // Check for collisions. Exit if found
+                if (s.collisionRect.Intersects(player.collisionRect))
+                {
+                    Game.Exit();
+                }
+            }
+
+            base.Update(gameTime);
+        }
 
         public override void Draw(GameTime gameTime)
         {
+            spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend);
+
+            // Draw Player
+            player.Draw(gameTime, spriteBatch);
+
+            // Draw sprite list
+            foreach (Sprite s in spriteList)
+            {
+                s.Draw(gameTime, spriteBatch);
+            }
+
+            spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
